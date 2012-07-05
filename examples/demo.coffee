067435dcd020a2c5c96f347a69016ac8f1f3c2a8
Tian <chentianwen@hotmail.com>
2012-07-05 22:26:24 +1000

    remove the try catch block. show me the exception plz

--- examples/demo.coffee
+++ examples/demo.coffee
@@ -2,11 +2,8 @@ jQuery ($) ->
   $('h1.title').text($.in_field.name + ' ' + $.in_field.version)
   prettyPrint()
 
-  try
-    interval_id = setInterval ->
-      build_status = $('#spec').contents().find('body').attr('data-build-status')
-      # build_status = window.frames['spec'].document.body.getAttribute('data-build-status')
-      $('#jasmine-build-status').removeClass('passed failed').addClass build_status
-    , 1000
-  finally
-    clearInterval interval_id
+  interval_id = setInterval ->
+    build_status = $('#spec').contents().find('body').attr('data-build-status')
+    # build_status = window.frames['spec'].document.body.getAttribute('data-build-status')
+    $('#jasmine-build-status').removeClass('passed failed').addClass build_status
+  , 1000
--- examples/demo.js
+++ examples/demo.js
@@ -4,15 +4,11 @@
     var interval_id;
     $('h1.title').text($.in_field.name + ' ' + $.in_field.version);
     prettyPrint();
-    try {
-      return interval_id = setInterval(function() {
-        var build_status;
-        build_status = $('#spec').contents().find('body').attr('data-build-status');
-        return $('#jasmine-build-status').removeClass('passed failed').addClass(build_status);
-      }, 1000);
-    } finally {
-      clearInterval(interval_id);
-    }
+    return interval_id = setInterval(function() {
+      var build_status;
+      build_status = $('#spec').contents().find('body').attr('data-build-status');
+      return $('#jasmine-build-status').removeClass('passed failed').addClass(build_status);
+    }, 1000);
   });
 
 }).call(this);
